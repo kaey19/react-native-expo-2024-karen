44ea6388a98b9afc6e30c037a5e7c5080c2e789c
kaey19 <karenyukariyt@gmail.com>
2024-10-16 12:39:54 +0000

    [feat] Async Storage Users

--- src/hooks/Auth/index.js
+++ src/hooks/Auth/index.js
@@ -1,13 +1,16 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import {useUsersDatabase} from "../../database/useUsersDatabase"; 
+import { useUsersDatabase } from "../../database/useUsersDatabase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ActivityIndicator, Text, View } from "react-native";
 
-const AuthContex = createContext({});
+const AuthContext = createContext({});
 
 export const Role = {
   SUPER: "SUPER",
   ADMIN: "ADMIN",
   USER: "USER",
 };
+
 export function AuthProvider({ children }) {
   const [user, setUser] = useState({
     autenticaded: null,
@@ -16,6 +19,32 @@ export function AuthProvider({ children }) {
   });
 
   const { authUser } = useUsersDatabase();
+
+  useEffect(() => {
+    const loadStoragedData = async () => {
+      const storagedUser = await AsyncStorage.getItem("@payment:user");
+
+      if (storagedUser) {
+        setUser({
+          autenticaded: true,
+          user: JSON.parse(storagedUser),
+          role: JSON.parse(storagedUser).role,
+        });
+      } else {
+        setUser({
+          autenticaded: false,
+          user: null,
+          role: null,
+        });
+      }
+    };
+
+    loadStoragedData();
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect: ", user);
+  }, [user]);
 
   const signIn = async ({ email, password }) => {
     const response = await authUser({ email, password });
@@ -30,18 +59,17 @@ export function AuthProvider({ children }) {
       throw new Error("Usuário ou senha inválidos");
     }
 
-    
+    await AsyncStorage.setItem("@payment:user", JSON.stringify(response));
 
     setUser({
       autenticaded: true,
       user: response,
       role: response.role,
     });
-
-       
   };
 
   const signOut = async () => {
+    await AsyncStorage.removeItem("@payment:user");
     setUser({});
   };
 
@@ -49,15 +77,26 @@ export function AuthProvider({ children }) {
     console.log("AuthProvider: ", user);
   }, [user]);
 
+  if (user?.autenticaded === null) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style={{fontSize: 28 }}>
+          Carregando Dados do Usuário
+        </Text>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  } 
+  
   return (
-    <AuthContex.Provider value={{ user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user, signIn, signOut }}>
       {children}
-    </AuthContex.Provider>
+    </AuthContext.Provider>
   );
 }
 
 export function useAuth() {
-  const context = useContext(AuthContex);
+  const context = useContext(AuthContext);
   if (!context) {
     throw new Error("useAuth must be used within an AuthProvider");
   }
