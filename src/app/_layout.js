81cc683025ee08994f3a79c8f7ddcca7f00c8fae
kaey19 <karenyukariyt@gmail.com>
2024-10-16 12:37:22 +0000

    [feat] Async Storage Users

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -5,26 +5,20 @@ import { useEffect } from "react";
 
 const StackLayout = () => {
   const { user } = useAuth();
-  const segments = useSegments();
 
   useEffect(() => {
-    const inAuthGroup = segments[0] === "(protected)";
-
-    if (!user?.autenticaded && inAuthGroup) {
-      if (router.canGoBack()) {
-        router.back();
-      }
+    if (!user?.autenticaded) {
+      router.replace("signin")
     } else {
-      if (user?.autenticaded) {
-        router.push("(protected)");
-      }
+      router.replace("(protected)");
     }
   }, [user]);
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }}/>
-      <Stack.Screen name="(protected)" options={{ headerShown: false }}/>
+      <Stack.Screen name="signin" options={{ headerShown: false }} />
+      <Stack.Screen name="about" options={{ headerShown: false }} />
+      <Stack.Screen name="(protected)" options={{ headerShown: false }} />
     </Stack>
   );
 };
--- src/app/about.jsx
+++ src/app/about.jsx
@@ -5,7 +5,7 @@ export default function About () {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: 'center', backgroundColor: '#FFF5E1' }}>
             <Text>Sobre</Text>
-            <Button title="Voltar" onPress={() => {router.replace("/")}} />
+            <Button title="Voltar" onPress={() => {router.replace("signin")}} />
         </View>
     );
 }
